96d25039d4c8beb6acfb0e56239f36f2fbe38d2b
Diana <diana@Serbans-MacBook-Pro.local>
2020-10-12 14:27:22 +0300

    Changed port for client side

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,15 +3,10 @@ import { text } from "body-parser";
 
 // event.preventDefault()
 let allData = {};
-    //let form = document.querySelector('form');
-    //const geoNamesUrl = 'http://api.geonames.org/searchJSON?q=';
-    //const username = "diana_szalai";
-    //const weatherbitUrl= 'https://api.weatherbit.io/v2.0/forecast/daily?';
-    //const ApiKey= '6c93fef169ea443692a449b79c652cf8';
-    //const pixabayUrl = 'https://pixabay.com/api/?key=';
-    //const pixabayAPI = '17209347-c5f003843d069024e54cd72f8';
     const foodUrl= 'https://api.spoonacular.com/recipes/complexSearch?apiKey='
-    const ApiKey =  '49c37fe7674b4a0aada0534814cd0333';
+    //const ApiKey =  '49c37fe7674b4a0aada0534814cd0333';
+   // const ApiKey =  '7ec74b107fba44ee83fb5886d1282e6f'
+    const ApiKey = 'c4d6cc06367049f190047e8d4a69953c'
     const idFoodUrl = 'https://api.spoonacular.com/recipes/'
 
 export function handleSubmit(e) {
@@ -23,18 +18,8 @@ export function handleSubmit(e) {
         let text = document.getElementById('name').value;
         let crbs = document.getElementById('carbs').value;
         let lang = document.getElementById('cuisine').value;
-        //const departure =new Date(document.getElementById('departure_date').value );
-        //const arrival =new Date(document.getElementById('arrival_date').value );
         let apiURL = foodUrl + ApiKey + '&query=' + text + '&maxCarbs=' + crbs + '&cuisine=' +lang ;
         console.log(apiURL)
-        //console.log(`This is to see the date`)
-        //var startDate = departure.getTime();
-        //var endDate = arrival.getTime();
-        //var stampDate= Math.floor((endDate-startDate) / (60 * 60 *  24 * 1000));
-        //allData.stampDate = stampDate
-        //console.log(`Calculate the diff:`)
-        //console.log(stampDate)
-         //Check the input destination 
         if (text.length !== 0){
         try { 
             // Fetching geo stats of destination place.
@@ -42,15 +27,10 @@ export function handleSubmit(e) {
                 .then((toInfo) => {
                     //Assigning the fetched value from JSON toInfo
                     console.log('2')
-                    console.log(toInfo)
-                    console.log(`----------This is the URL FOR FOOD`)
                     console.log(apiURL);
-                    console.log (toInfo);
                     const idNumber = toInfo.results[0].id;
                     const title = toInfo.results[0].title;
                     let someData = toInfo.results; 
-                    console.log('------------important')
-                    console.log(someData)
                         let recData = {
                             accounting: []
                     };
@@ -62,19 +42,11 @@ export function handleSubmit(e) {
                             "image" : item.image
                         });
                     };
-                    console.log('------------important')
-                    console.log(recData.accounting)
-                    console.log('----------very--important')
                     let arrayResults = recData.accounting
                     console.log(arrayResults)
                     allData.accounting= arrayResults;
-
-                    //const latt = toInfo.geonames[0].lat;
-                    //const long = toInfo.geonames[0].lng;
-                    //allData.name = toInfo.geonames[0].name;
-                    //allData.countryCode = toInfo.geonames[0].countryCode
-                    //console.log(latt, long)
                     console.log(idNumber, title )
+
                     //Getting Weather details
                     return getWeatherData(idNumber, title ) 
                 })
@@ -92,12 +64,9 @@ export function handleSubmit(e) {
                     //Calling Pixabay API to fetch the first img of the city
                     return getImage(allData.destination);
                 })
-               // .then((allData) => {
-                    //if (imageDetails.hits.length > 0) {
-                      //  allData.cityImage = imageDetails.hits[0].webformatURL ;
-                        updateInfo(allData);
-                  //  }
-               // })
+                console.log(`------------>>>>>>problem`)
+                console.log(allData)        
+                updateInfo(allData);
         } catch (e) {
             console.log('error', e);
         }
@@ -158,20 +127,24 @@ async function getImage(city) {
 
 //Updating the UI
 function updateInfo(allData) {
-    document.getElementById('myAnchor').src = allData.image;
-    document.getElementById('recipe1').src = allData.accounting[1].image;
-    document.querySelector('.cityName').textContent = allData.title;
-    document.querySelector('.recipe-1').textContent = allData.accounting[1].title;
-    document.querySelector('.recipe-2').textContent = allData.accounting[2].title;
-    document.querySelector('.recipe-3').textContent = allData.accounting[3].title;
-    document.querySelector('.recipe-4').textContent = allData.accounting[4].title;
-    document.querySelector('.recipe-5').textContent = allData.accounting[5].title;
-    document.querySelector('.recipe-5').textContent = allData.accounting[5].title;
-    document.querySelector('.countryCode').textContent = allData.summary
-    //document.getElementById('iconID').src = `/src/client/views/icons/${allData.weatherIcon}.png`;
-    //console.log(`/src/client/views/icons/${allData.weatherIcon}.png`)
-    //document.querySelector('.temperature').textContent =`${allData.temperature} °C` ;
-    //document.querySelector('.weatherDescription').textContent = allData.weatherDesc;
-    //document.querySelector('.travelTime').textContent = allData.stampDate;
-} 
+    console.log(`_________________->>>>>`)
+    console.log(allData)
+    //document.querySelector('.cityName').textContent = allData.title;
+        let finalArray = [];
+        allData.accounting.forEach(element => {
 
+            if (!finalArray.includes(element.title)){
+                finalArray.push(element.title);
+         }
+            console.log(`-------->>>>>>`+ finalArray)
+
+            var node = document.createElement("LI");
+            var img = document.createElement('img');
+            document.querySelector('.cityImg').appendChild(img);
+            img.src = element.image
+            var textnode = document.createTextNode(element.title);
+            node.appendChild(textnode);
+            document.getElementById("myList").appendChild(node);
+    })
+    //document.querySelector('.countryCode').textContent = allData.summary    
+} 
